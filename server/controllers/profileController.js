43dd40e058bd6eaccf806c4f0baf3c64c1ca6c02
Varun <157891005+varunbalaji167@users.noreply.github.com>
2025-06-01 18:02:17 +0530

    Cleared Bugs

--- server/controllers/profileController.js
+++ server/controllers/profileController.js
@@ -1,6 +1,5 @@
 // server/controllers/profileController.js
 const Dentist = require("../models/dentistModel");
-const Reception = require("../models/receptionModel");
 const User = require("../models/userModel");
 const DentistSchedule = require('../models/dentistScheduleModel');
 
@@ -119,60 +118,6 @@ exports.getAllDentists = async (req, res) => {
     res.status(500).json({
       message: "Error retrieving dentist profiles",
       error: error.message
-    });
-  }
-};
-
-// To create a new receptionist profile 
-exports.createReceptionistProfile = async (req, res) => {
-  const { receptionistId, name, phone_no, salary } = req.body;
-
-  try {
-    // Find the user by receptionistId
-    const user = await User.findOne({ receptionistId });
-    if (!user || user.role !== "Receptionist") {
-      return res
-        .status(404)
-        .json({ message: "User not found or not a receptionist" });
-    }
-
-    // Check if a receptionist profile already exists
-    let receptionistProfile = await Reception.findOne({
-      repID: receptionistId,
-    });
-
-    if (receptionistProfile) {
-      // Update existing profile
-      receptionistProfile.name = name;
-      receptionistProfile.phone_no = phone_no;
-      receptionistProfile.salary = salary;
-      receptionistProfile.email = user.email; // Use the email from the user
-
-      await receptionistProfile.save(); // Save the updated profile
-      return res.status(200).json({
-        message: "Receptionist profile updated successfully",
-        receptionistProfile,
-      });
-    } else {
-      // Create a new receptionist profile
-      receptionistProfile = new Reception({
-        repID: receptionistId, // Use receptionistId as repID
-        name,
-        phone_no,
-        email: user.email, // Use the email from the user
-        salary,
-      });
-
-      await receptionistProfile.save(); // Save the new profile
-      return res.status(201).json({
-        message: "Receptionist profile created successfully",
-        receptionistProfile,
-      });
-    }
-  } catch (error) {
-    res.status(500).json({
-      message: "Error creating or updating receptionist profile",
-      error: error.message,
     });
   }
 };
